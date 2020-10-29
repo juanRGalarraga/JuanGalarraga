0bf41b922c638d9d17aaa4eb0469dc66087186f8
Juan <juan_dlp2@outlook.com>
2020-10-29 18:36:14 -0300

    modificacion de archivos

--- app.js
+++
@@ -1,9 +0,0 @@
-var btn = document.getElementById('b1');
-var ip1 = document.getElementById('ip1');
-var ip2 = document.getElementById('ip2');
-
-btn.addEventListener('click', showMessage);
-
-function showMessage(){
-    alert("Hola Mundo");
-}
